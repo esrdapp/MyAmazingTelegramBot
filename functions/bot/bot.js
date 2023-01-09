920c7a980504cf751da9f4ce96bb672a9e7b345b
ESR <82104698+esrdapp@users.noreply.github.com>
2023-01-09 16:52:13 +0000

    Update bot.js

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -4,10 +4,10 @@ const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start(ctx => {
   console.log("Received /start command")
   try {
-    return ctx.reply("Congrats! You've connected to Netlify!")
+      ctx.reply("Congrats! You've connected to Netlify!")
   } catch (e) {
     console.error("error in start action:", e)
-    return ctx.reply("Error occured")
+    ctx.reply("Error occured")
   }
 })
 
